ab3d40ad8aac0c25e31aa4adca03b2e5c8161420
a00721293 <andytung215@hotmail.com>
2014-06-18 20:36:43 -0700

    midterm

--- midterm/src/ca/bcit/comp2613/a00721293/wow/DontHaveTheExpansionException.java
+++ midterm/src/ca/bcit/comp2613/a00721293/wow/DontHaveTheExpansionException.java
@@ -1,13 +1,9 @@
 package ca.bcit.comp2613.a00721293.wow;
 
+public class DontHaveTheExpansionException extends Exception {
 
-public class DontHaveTheExpansionException extends Exception
-{
-
-	public DontHaveTheExpansionException(String string)
-	{
+	public DontHaveTheExpansionException(String string) {
 		// TODO Auto-generated constructor stub
 	}
 
 }
-
--- midterm/src/ca/bcit/comp2613/a00721293/wow/Race.java
+++ midterm/src/ca/bcit/comp2613/a00721293/wow/Race.java
@@ -1,8 +1,6 @@
 package ca.bcit.comp2613.a00721293.wow;
 
-public enum Race 
-{
+public enum Race {
 	WORGEN, DRAENEI, DWARF, GNOME, ORC, PANDAREN, HUMAN
 	// TODO, add a HUMAN Race
 }
-
